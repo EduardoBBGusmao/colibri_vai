5c0e407f04ece74c56bc642b7e3bdbabaed73796
Eduardo Gusmão Filho <42840856+EduardoBBGusmao@users.noreply.github.com>
2019-06-20 11:31:35 -0300

    Update stdcsv.c

--- stdcsv.c
+++ stdcsv.c
@@ -14,18 +14,18 @@ struct car* read_line(struct car* car_p)
 	char line[128];
 	FILE *new_file;
 	new_file = fopen(vai_file, "r");
-	if(!new_file){
+	if (!new_file) {
 	        printf("Error when opening file\n");
 	        exit(1);
 	}
-	while (fgets (line, sizeof line, new_file) != NULL){
+	while (fgets (line, sizeof line, new_file) != NULL) {
 		char *ptr = strtok(line, csv_delimiter);
 		char *headers[9];
 		float headers_float[5];
 		int i=0;
 
 
-		while(ptr != NULL){	
+		while (ptr != NULL) {	
 			headers[i] = ptr;
 			i++;
 			ptr = strtok(NULL, csv_delimiter);
@@ -41,7 +41,7 @@ struct car* read_line(struct car* car_p)
 	return car_p;
 }
 
-struct car* insert_bottom(float headers_float[5], char* headers[4],struct car* head) 
+struct car* insert_bottom(float headers_f[5],char* headers[4],struct car* head) 
 {
 	struct car* current_node = head;
 	struct car* new_node = (struct car*)malloc(sizeof(struct car));
@@ -51,21 +51,20 @@ struct car* insert_bottom(float headers_float[5], char* headers[4],struct car* h
         char* dongle_id = (char*)malloc(20*sizeof(char));
         char* customer = (char*)malloc(20*sizeof(char));
         
-        if (!dongle_id){
+        if (!dongle_id) {
         	printf("allocation not possible");
         	exit(1);
         } 
-        if (!customer){
+        if (!customer) {
         	printf("allocation not possible");
         	exit(1);
         }
-        if (!new_node){
+        if (!new_node) {
         	printf("allocation not possible");
         	exit(1);
         }
      	
         strcpy(dongle_id, headers[0]);
-        
         strcpy(customer, headers[1]);
    
         while (current_node != NULL && current_node->next_car != NULL) {
@@ -76,22 +75,20 @@ struct car* insert_bottom(float headers_float[5], char* headers[4],struct car* h
 	new_node -> customer = customer;
 	new_node -> started_at = date_start;
 	new_node -> finished_at = date_end;
-	new_node -> minute = headers_float[0];
-	new_node -> consumption = headers_float[1];
-	new_node -> mileage = headers_float[2];
-        new_node -> cost = headers_float[3];
-        new_node -> kml = headers_float[4];
+	new_node -> minute = headers_f[0];
+	new_node -> consumption = headers_f[1];
+	new_node -> mileage = headers_f[2];
+        new_node -> cost = headers_f[3];
+        new_node -> kml = headers_f[4];
 	new_node -> trip_info = trip_average(headers[2]);
 	new_node -> next_car= NULL;
 	
-	if (current_node != NULL){
+	if (current_node != NULL)
 		current_node->next_car = new_node;
-	} else {
+	else
 		return new_node;
-	}
 	
 	return head;
-
 }
 
 void print_list(struct car* car_info)
@@ -101,8 +98,7 @@ void print_list(struct car* car_info)
 	while (temp != NULL){
 		print_node(temp);
 		temp = temp -> next_car;
-	};
-	
+	}	
 }
 
 void print_node(struct car* temp)
@@ -133,35 +129,35 @@ struct trip trip_average(char* reference)
 	new_file = fopen(trip_file, "r");
 	struct trip current_average;
 
-	if(!new_file){
+	if (!new_file) {
 	        printf("Error to open file ");
 	        exit(1);
 	}
-
 	char line[128];
 
 	fgets (line, sizeof line, new_file);
 
-	while (fgets (line, sizeof line, new_file) != NULL){
+	while (fgets (line, sizeof line, new_file) != NULL) {
 
 		char *ptr = strtok(line, csv_delimiter);
 		char *headers[4];
 
-		while(ptr != NULL && i < 3){
+		while (ptr != NULL && i < 3) {
 			headers[i] = ptr;
 			i++;
 			ptr = strtok(NULL, csv_delimiter);
-		};
-		if (!strcmp(headers[0], reference)){ 			
+		}
+		if (!strcmp(headers[0], reference)) { 			
 			current_speed = atof(headers[1]);
 			current_rpm = atof(headers[2]);
 			
-			if (current_speed != 0){
+			if (current_speed != 0) {
 		        	count++;
-		        	a_speed = (a_speed*(count-1) + current_speed)/count;
+		        	int var = a_speed*(count-1);
+		        	a_speed = (var + current_speed)/count;
 		        	a_rpm = (a_rpm*(count-1) + current_rpm)/count;
 			}
-			i=0;
+			i = 0;
 		}
 	}
 
@@ -170,29 +166,29 @@ struct trip trip_average(char* reference)
 	current_average.average_speed = a_speed;
 	current_average.average_rpm = a_rpm;
 	return current_average;	
-
 }
 
-char* convert_date(char* string)
+char* convert_date (char* string)
 {
 	char* date = malloc(11*sizeof(char)); 
-	if (!date){
+	if (!date) {
         	printf("allocation not possible");
         	exit(1);
         }
 	
-	for(int j = 0; j < 10; j++){
+	for(int j = 0; j < 10; j++) {
 		date[j] = string[j];
 	}
 	date[10] = '\0';
-	return date;
 	
+	return date;
 }
 
 void free_list(struct car* car_info)
 {
         struct car* curr = car_info;
-	if(curr != NULL){
+        
+	if (curr != NULL) {
 	        car_info = car_info -> next_car;
 	        free_list(car_info);
 	        free(curr->dongle);
